06743bfdd874167d0001aa4857a9911d0079e154
dzmitry <demidov91@mail.ru>
2019-01-21 16:09:58 +0300

    Brush up backup logic a little.

--- byn/tasks/backup.py
+++ byn/tasks/backup.py
@@ -80,7 +80,7 @@ def backup_external_rates_async():
 @app.task
 def _create_cassandra_table_backup(table: str, columns: Tuple[str]):
     data = db.execute(f'SELECT {", ".join(columns)} from {table}')
-    with gzip.open(f'/tmp/{table}.csv', mode='wt') as f:
+    with gzip.open(f'/tmp/{table}.csv.gz', mode='wt') as f:
         writer = csv.writer(f, delimiter=';')
         writer.writerow(columns)
         writer.writerows([tuple(x) for x in data])
@@ -90,7 +90,7 @@ def _create_cassandra_table_backup(table: str, columns: Tuple[str]):
 def _send_cassandra_table_backup_to_s3(table: str):
     s3 = boto3.client('s3')
     s3.upload_file(
-        f'/tmp/{table}.csv',
+        f'/tmp/{table}.csv.gz',
         const.BACKUP_BUCKET,
         f'{table}_{datetime.date.today():%Y-%m-%d}.csv.gz'
     )
