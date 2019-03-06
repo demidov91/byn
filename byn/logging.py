bb3b08dd38a205c1bf60fd03da0671b9b9efea71
dzmitry <demidov91@mail.ru>
2019-03-06 14:02:55 +0300

    Add sentry usage.

--- byn/logging.py
+++ byn/logging.py
@@ -1,4 +1,24 @@
 import logging
+import os
+
+import sentry_sdk
+from sentry_sdk.integrations.logging import LoggingIntegration
+from sentry_sdk.integrations.celery import CeleryIntegration
+from sentry_sdk.integrations.aiohttp import AioHttpIntegration
 
 
 logging.basicConfig(level=logging.DEBUG)
+
+
+sentry_sdk.init(
+    dsn=os.environ['SENTRY_DSN'],
+    environment=os.environ['SENTRY_ENVIRONMENT'],
+    integrations=[
+        LoggingIntegration(
+            level=logging.DEBUG,
+            event_level=logging.WARNING
+        ),
+        CeleryIntegration(),
+        AioHttpIntegration(),
+    ],
+)
